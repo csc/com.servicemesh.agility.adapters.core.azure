757dc28e4206cbdea9db2782b7b937f832ae8685
Dustin Behnke <dustin.behnke@servicemesh.com>
2016-06-14 15:46:22 -0500

    Update build to publish artifacts to bintray

---
+++ publishToBintray.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Uploading artifacts to bintray at https://bintray.com/csc/opensource/com.servicemesh.agility.adapters.core.azure"
+
+cd dist
+
+shopt -s nullglob
+for file in *;
+do
+   curl -T $file -u$2:$3 "https://api.bintray.com/content/csc/opensource/com.servicemesh.agility.adapters.core.azure/$1/$file?publish=1&override=1"
+done
+
+
